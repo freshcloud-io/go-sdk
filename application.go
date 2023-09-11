988e0ee74b2c0bc1f3c8cf8d7b9ef3d625cba5ec
Davide Berdin <davideberdin@gmail.com>
2023-09-11 10:52:52 +0200

    Revert "remove hashicorp go-plugin"

--- application.go
+++ application.go
@@ -2,9 +2,101 @@ package fresh
 
 import (
 	"fmt"
+	"net/rpc"
 
 	"github.com/freshcloud-io/go-sdk/utils"
+	"github.com/hashicorp/go-plugin"
 )
+
+const (
+	PluginNameApplication       string = "fresh-application"
+	MagicCookieKeyApplication   string = "FRESH_APPLICATION_PLUGIN"
+	MagicCookieValueApplication string = "4ab23e44-f2c4-44c2-9b66-448d78d4713c"
+)
+
+func NewFresherApplication(f FreshApplication) error {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: plugin.HandshakeConfig{
+			ProtocolVersion:  1,
+			MagicCookieKey:   MagicCookieKeyApplication,
+			MagicCookieValue: MagicCookieValueApplication,
+		},
+		Plugins: map[string]plugin.Plugin{
+			PluginNameApplication: &FreshApplicationPlugin{Impl: f},
+		},
+	})
+	return nil
+}
+
+type FreshApplication interface {
+	// EntryPoint defines the "main" function
+	EntryPoint() error
+	// Shutdown defines the place to tear down things where necessary
+	Shutdown() error
+	GetApplication() *Application
+}
+
+type FreshApplicationRPC struct {
+	client *rpc.Client
+}
+
+func (f *FreshApplicationRPC) EntryPoint() error {
+	var resp error
+	err := f.client.Call("Plugin.EntryPoint", new(interface{}), &resp)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+func (f *FreshApplicationRPC) Shutdown() error {
+	var resp error
+	err := f.client.Call("Plugin.Shutdown", new(interface{}), &resp)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+func (f *FreshApplicationRPC) GetApplication() *Application {
+	var resp *Application
+	err := f.client.Call("Plugin.GetApplication", new(interface{}), &resp)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+type FreshApplicationRPCServer struct {
+	Impl FreshApplication
+}
+
+func (s *FreshApplicationRPCServer) EntryPoint(args interface{}, resp *error) error {
+	*resp = s.Impl.EntryPoint()
+	return nil
+}
+
+func (s *FreshApplicationRPCServer) Shutdown(args interface{}, resp *error) error {
+	*resp = s.Impl.Shutdown()
+	return nil
+}
+
+func (s *FreshApplicationRPCServer) GetApplication(args interface{}, resp *Application) error {
+	*resp = *s.Impl.GetApplication()
+	return nil
+}
+
+type FreshApplicationPlugin struct {
+	Impl FreshApplication
+}
+
+func (p *FreshApplicationPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
+	return &FreshApplicationRPCServer{Impl: p.Impl}, nil
+}
+
+func (FreshApplicationPlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &FreshApplicationRPC{client: c}, nil
+}
 
 type ApplicationOption func(*Application)
 
--- cron.go
+++ cron.go
@@ -2,9 +2,90 @@ package fresh
 
 import (
 	"fmt"
+	"net/rpc"
 
 	"github.com/freshcloud-io/go-sdk/utils"
+	pb "github.com/freshcloud-io/protos/go/freshcloud"
+	"github.com/hashicorp/go-plugin"
 )
+
+const (
+	PluginNameCron       string = "fresh-cron"
+	MagicCookieKeyCron   string = "FRESH_CRON_PLUGIN"
+	MagicCookieValueCron string = ""
+)
+
+func NewFresherCron(f FreshCron) error {
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: plugin.HandshakeConfig{
+			ProtocolVersion:  1,
+			MagicCookieKey:   MagicCookieKeyCron,
+			MagicCookieValue: MagicCookieValueCron,
+		},
+		Plugins: map[string]plugin.Plugin{
+			PluginNameCron: &FreshCronPlugin{Impl: f},
+		},
+	})
+	return nil
+}
+
+type FreshCron interface {
+	Execute(args *pb.CronExecuteRequest) *pb.CronExecuteResponse
+	GetCron() *Cron
+}
+
+type FreshCronRPC struct {
+	client *rpc.Client
+}
+
+func (f *FreshCronRPC) Execute(args *pb.CronExecuteRequest) *pb.CronExecuteResponse {
+	var resp *pb.CronExecuteResponse
+	err := f.client.Call("Plugin.Execute", args, &resp)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+func (f *FreshCronRPC) GetCron() *Cron {
+	var resp *Cron
+	err := f.client.Call("Plugin.GetCron", new(interface{}), &resp)
+	if err != nil {
+		panic(err)
+	}
+	return resp
+}
+
+type FreshCronRPCServer struct {
+	Impl FreshCron
+}
+
+func (s *FreshCronRPCServer) Execute(args interface{}, resp *pb.CronExecuteResponse) error {
+	a, ok := args.(*pb.CronExecuteRequest)
+	if !ok {
+		return fmt.Errorf("wrong cron arguments conversion")
+	}
+
+	*resp = *s.Impl.Execute(a)
+	return nil
+}
+
+func (s *FreshCronRPCServer) GetCron(args interface{}, resp *Cron) error {
+	*resp = *s.Impl.GetCron()
+	return nil
+}
+
+type FreshCronPlugin struct {
+	Impl FreshCron
+}
+
+func (p *FreshCronPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
+	return &FreshCronRPCServer{Impl: p.Impl}, nil
+}
+
+func (FreshCronPlugin) Client(b *plugin.MuxBroker, c *rpc.Client) (interface{}, error) {
+	return &FreshCronRPC{client: c}, nil
+}
 
 type CronOption func(*Cron)
 
