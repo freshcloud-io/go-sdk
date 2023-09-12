1defbef17fb5bb20ee62bb828d54fa58ed5493f3
Davide Berdin <davideberdin@gmail.com>
2023-09-12 12:14:46 +0200

    remove zerolog

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.20
 
 require (
 	github.com/freshcloud-io/protos v0.1.0
-	github.com/hashicorp/go-plugin v1.5.1
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/hashicorp/go-plugin v1.5.1
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
@@ -20,7 +20,6 @@ require (
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-testing-interface v0.0.0-20171004221916-a61a99592b77 // indirect
 	github.com/oklog/run v1.0.0 // indirect
-	github.com/rs/zerolog v1.30.0
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
--- grpc/grpc.go
+++ grpc/grpc.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/freshcloud-io/go-sdk/utils"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
-	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
@@ -31,7 +30,6 @@ func NewConnection(bindAddr string, port int) (*Connection, error) {
 		retry.WithMax(3),
 		retry.WithBackoff(retry.BackoffLinear(retryTimeout)),
 	)
-	log.Debug().Msg("initialize gRPC client connection")
 	conn, err := grpc.Dial(rpcAddr,
 		grpc.WithBlock(), // this helps with having the grpc client waiting for the server to be avaialble and retry
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -46,6 +44,5 @@ func NewConnection(bindAddr string, port int) (*Connection, error) {
 }
 
 func (c *Connection) CloseConn() error {
-	log.Debug().Msg("close gRCP client connection of Fresh Cloud")
 	return c.Close()
 }
